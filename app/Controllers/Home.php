ce690b840121073a8a8627158b7be67fdb81fe83
Manish JainP <manish@needsmet.org>
2023-07-26 16:00:37 +0530

    written code to get data and show in user view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,35 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
+use App\Models\UserModel;
 
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('welcome_message');
-	}
+    public function __construct(){
+        //   loding user model
+        $this->user = new UserModel();       
+    }
 
-	//--------------------------------------------------------------------
+    public function index()
+    {
+        return view('welcome_message');
+    }
 
+    /**
+     * 
+     */
+    public function getUsers()
+    { 
+        /**
+         *  @var data
+         *  @type array
+         *  @use - to collect all data that need to show/use in view
+         *  @return view with array data
+         */
+        $data = array();
+
+        $data['users'] = $this->user->findAll();
+
+        return view('users', $data);
+    }
 }
